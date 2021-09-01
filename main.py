0bc6a06b9c7ac678522583ca029eb36f299614a2
omarbaumgartner <omar.baumgartner@gmail.com>
2021-09-01 14:43:15 +0200

    Adding delay between requests

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests,time,env
 import methods as m
 import socket
 from urllib3.connection import HTTPConnection
+from random import randint
+from time import sleep
 
 
 # ... Making the program resilient ( Avoid interruption of the program )
@@ -17,7 +19,6 @@ HTTPConnection.default_socket_options = (
 
 m.init()
 
-count = 0 # Downloads counter
 startingTime = int(round(time.time()))
 
 # Opening the list of already used urls
@@ -27,62 +28,69 @@ with open(env.existingUrlsFileName, 'r') as ListFile:
 with open(env.nonexistingUrlsFileName, 'r') as ListFile:
     NonExistingList = ListFile.read().split("\n")
 
+proxies={'https':'http://62.252.146.74:8080'}
 # Creating session
 session = requests.Session()
 # Added a User-Agent otherwise it would'nt be retrieved
 session.headers.update(
         {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'})
-    
+#session.proxies = proxies    
 
-while(True):
-    # LightShot url has 6 caracters ( a-z and 0-9 )
-    RandomUrl = m.RandomUrl()
-    # Checking if generated URL is already known
-    ExistingPathComparisonBool = m.CompareUrl(ExistingList, RandomUrl)
-    NonExistingPathComparisonBool = m.CompareUrl(NonExistingList, RandomUrl)
-    # If it's already known, we generate a new URL
-    while(ExistingPathComparisonBool == True & NonExistingPathComparisonBool == True):
+def main():
+    count = 0 # Downloads counter
+    while(True):
+        # LightShot url has 6 caracters ( a-z and 0-9 )
         RandomUrl = m.RandomUrl()
+        # Checking if generated URL is already known
         ExistingPathComparisonBool = m.CompareUrl(ExistingList, RandomUrl)
         NonExistingPathComparisonBool = m.CompareUrl(NonExistingList, RandomUrl)
-    
-    # Sending an HTML GET Request to LightShow's website to retrieve its HTML page.
-    content = session.get('https://prnt.sc/'+RandomUrl)
-    
-    # Retrieving the portion of URL which defines the screenshot
-    ImageUrl = m.GetImageUrl(content.text)
-    
-    # Return false if the portion isn't found
-    if(ImageUrl == False):
-        NonExistingList.append(ImageUrl)
-        # Adding the useless URL in a list.
-        with open(env.nonexistingUrlsFileName, 'a') as nonExistingListFile:
-            nonExistingListFile.write(RandomUrl+"\n")
-    else:
-        ExistingList.append(ImageUrl)
-        # Adding the already used URL in a list.
-        with open(env.existingUrlsFileName, 'a') as ExistingListFile:
-            filename = ImageUrl.split('/')[-1]
-            
-            # Download image --------
-            
-            # Getting image
-            #r = requests.get(ImageUrl, allow_redirects=True)
-            # Downloading file into directory
-            #open(env.downloadsDirectory+'/'+filename, 'wb').write(r.content)
-            
-            """
-            TODO : 
-            - Make the program only fetch correct urls without downloading them
-            - Add a method to directly download the images from the existinglist ( and download only the images that are not already in the directory ) 
-            - make ExistingList hosted online ( via google drive api ) to have only one single list ( even if using different machines ) 
-            """
-            # -------
+        # If it's already known, we generate a new URL
+        while(ExistingPathComparisonBool == True & NonExistingPathComparisonBool == True):
+            RandomUrl = m.RandomUrl()
+            ExistingPathComparisonBool = m.CompareUrl(ExistingList, RandomUrl)
+            NonExistingPathComparisonBool = m.CompareUrl(NonExistingList, RandomUrl)
+        # Sending an HTML GET Request to LightShow's website to retrieve its HTML page.
+        url = 'https://prnt.sc/'+RandomUrl
+        content = session.get(url)
+        # Retrieving the portion of URL which defines the screenshot
+        ImageUrl = m.GetImageUrl(content.text)
+        # Return false if the portion isn't found
+        if(ImageUrl == False):
+            print(RandomUrl,"not existing")
+            NonExistingList.append(ImageUrl)
+            # Adding the useless URL in a list.
+            with open(env.nonexistingUrlsFileName, 'a') as nonExistingListFile:
+                nonExistingListFile.write(RandomUrl+"\n")
+        else:
+            print("existing")
+            ExistingList.append(ImageUrl)
+            # Adding the already used URL in a list.
+            with open(env.existingUrlsFileName, 'a') as ExistingListFile:
+                filename = ImageUrl.split('/')[-1]
+                
+                # Download image --------
+                
+                # Getting image
+                #r = requests.get(ImageUrl, allow_redirects=True)
+                # Downloading file into directory
+                #open(env.downloadsDirectory+'/'+filename, 'wb').write(r.content)
+                
+                """
+                TODO : 
+                - Make the program only fetch correct urls without downloading them
+                - Add a method to directly download the images from the existinglist ( and download only the images that are not already in the directory ) 
+                - make ExistingList hosted online ( via google drive api ) to have only one single list ( even if using different machines ) 
+                """
+                # -------
 
-            ExistingListFile.write(RandomUrl+"\n")
-            count = count+1
-            actualTime = int(round(time.time()))
-            # How much screenshots per second you're downloading
-            ratio = count / (actualTime-startingTime)
-            print("Speed : ", ratio, "img/s")
-            print("Downloaded (", count, ")")
+                ExistingListFile.write(RandomUrl+"\n")
+                count = count+1
+                #actualTime = int(round(time.time()))
+                # How much screenshots per second you're downloading
+                #ratio = count / (actualTime-startingTime)
+                #print("Speed : ", ratio, "img/s")
+                print("Downloaded (", count, ")")        
+        sleep(randint(1,5))
+
+
+main()
